49d7528fbcb78fa1ced1308e119322eddf9e22ae
takenoko <ttaakkee@gmail.com>
2018-11-12 19:36:44 +0900

    Add load function

--- src/System/Plugins/Dynloader.hs
+++ src/System/Plugins/Dynloader.hs
@@ -1,4 +1,7 @@
-module System.Plugins.Dynloader where
+module System.Plugins.Dynloader (
+  unsafeLoad,
+  load
+  ) where
 
 import GHC hiding (ModuleName, load)
 import GHC.Paths (libdir)
@@ -6,16 +9,32 @@ import DynFlags (PackageFlag(..), PackageArg(..), ModRenaming(..))
 import Data.Dynamic (Dynamic)
 import Unsafe.Coerce
 
-unsafeLoad 
-  :: [PackageName]  -- ^ e.g. vector
-  -> [ModuleName] -- ^ e.g. Data.Vector
-  -> Expression -- ^ e.g. length
-  -> IO a
+-- |
+-- e.g.
+--
+-- @
+-- f <- unsafeload [] ["Prelude"] "(+)"
+-- f 1 2 -- 3
+-- @
+unsafeLoad :: [PackageName] -> [ModuleName] -> Expression -> IO a
 unsafeLoad pkgs mdls exp = runGhcT (Just libdir) $ do
-  initDynFlags
-  setContext $ map (IIDecl . simpleImportDecl . mkModuleName) mdls
+  initGhc pkgs mdls
   r <- compileExpr exp
   return $ unsafeCoerce r
+
+load :: [PackageName] -> [ModuleName] -> Expression -> IO Dynamic
+load pkgs mdls exp = runGhcT (Just libdir) $ do
+  initGhc pkgs mdls
+  dynCompileExpr exp
+
+type PackageName = String
+type ModuleName = String
+type Expression = String
+
+initGhc :: GhcMonad m => [PackageName] -> [ModuleName] -> m ()
+initGhc pkgs mdls = do
+  initDynFlags
+  setContext $ map (IIDecl . simpleImportDecl . mkModuleName) mdls
   where
     initDynFlags = do
       df <- getSessionDynFlags
@@ -24,7 +43,3 @@ unsafeLoad pkgs mdls exp = runGhcT (Just libdir) $ do
         ghcLink = LinkInMemory,
         packageFlags = [ExposePackage p (PackageArg p) (ModRenaming True []) | p <- pkgs]
         }
-
-type PackageName = String
-type ModuleName = String
-type Expression = String
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,12 +8,23 @@ import Data.Maybe
 
 main :: IO ()
 main = hspec $ do
-  it "can load foreign value"  $ do
+  describe "unasfeLoad" $ do
+    it "can load foreign value"  $
       unsafeLoad [] [] "3" `shouldReturn` 3
 
-  it "can load Prelude function"  $ do
+    it "can load Prelude function"  $
       do
         plus <- unsafeLoad [] ["Prelude"] "(+)"
         return $ plus 1 2
 
         `shouldReturn` 3
+
+  describe "load" $
+    it "can return Prelude data wraped in Dynamic"  $
+      do
+        dyn <- load [] ["Prelude"] "Just True"
+        let value = fromJust $ fromDynamic dyn
+        return $ value
+
+        `shouldReturn` Just True
+
